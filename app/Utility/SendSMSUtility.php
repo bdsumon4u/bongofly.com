c451e14c0a8a34e02ecee781f55a4b04ac0aaca4
Sumon Ahmed <bdsumon4u@gmail.com>
2023-01-22 19:13:42 +0600

    ElitBuzz SMS Utility

--- app/Utility/SendSMSUtility.php
+++ app/Utility/SendSMSUtility.php
@@ -204,6 +204,25 @@ class SendSMSUtility
                 $response = curl_exec($ch);
                 curl_close($ch);
             return $response;
+        } elseif (OtpConfiguration::where('type', 'elitbuzz')->first()->value == 1) {
+            $url = "https://msg.elitbuzz-bd.com/smsapi";
+            $data = [
+                "api_key" => env('ELITBUZZ_API_KEY'),
+                "type" => "text",
+                "contacts" => $to,
+                "senderid" => env('ELITBUZZ_USER_SENDER_ID'),
+                "label" => "transactional",
+                "msg" => $text,
+            ];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            return $response;
         }
         return true;
     }
